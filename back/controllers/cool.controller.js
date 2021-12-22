9d4f0a2280cfb447ae0e997c332025f80632e1f6
Timofey Myachikov <57thrasher57@gmail.com>
2021-12-22 12:38:42 +0500

    Now it's in azure

--- back/controllers/cool.controller.js
+++ back/controllers/cool.controller.js
@@ -45,7 +45,7 @@ exports.analyze = async (req, res) => {
     let analId = null;
     Parse.findOne({
         where: {
-            id: req.body.parseId
+            id: req.body.parse_id
         }
     }).then(anal => {
         Analysis.create({
@@ -55,126 +55,52 @@ exports.analyze = async (req, res) => {
             result: ''
         }).then((val) => {
             analId = val.id;
+            workNigga();
             return res.status(200).send({msg: 'In progress', analId});
         });
     });
 
-    function getToken(errorCallback, loadCallback) {
-        const req = new XMLHttpRequest();
-        req.addEventListener("load", loadCallback);
-        req.addEventListener("error", errorCallback);
-        req.open("POST", "https://iam.cloud.ibm.com/identity/token");
-        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        req.setRequestHeader("Accept", "application/json");
-        req.send("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" + config.ibmToken);
-    }
 
-    function apiPost(scoring_url, token, payload, loadCallback, errorCallback) {
-        const oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", loadCallback);
-        oReq.addEventListener("error", errorCallback);
-        oReq.open("POST", scoring_url);
-        oReq.setRequestHeader("Accept", "application/json");
-        oReq.setRequestHeader("Authorization", "Bearer " + token);
-        oReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        oReq.send(payload);
-    }
-
-    function getFinal(response, tokenResponse) {
-        const cats = [
-            "Category_attempted to attempt",
-            "Category_disease/mental disorders/affective disorder",
-            "Category_disease/mental disorders/self-destructive behavior/suicide",
-            "Category_disease/nervous system diseases/bad",
-            "Category_disease/nervous system diseases/pain",
-            "Category_movies/film genres/anime/animal/invertebrates",
-            "Category_people",
-            "Category_psychology/behavior and behavior mechanisms/emotions",
-            "Category_psychology/behavior and behavior mechanisms/emotions/love",
-            "Category_psychology/psychological phenomena and processes/mental processes/learning/better"
-        ];
-        let fields = '';
-        let values = '';
-        cats.forEach((el) => {
-            fields += `"${el}" ,`;
-            values += `"${response.predictions[0].values[0][response.predictions[0].fields.indexOf(el)]}" ,`;
-        });
-        const newPayload = `{"input_data": [{"fields": [${fields.slice(0, -1)}], "values": [[ ${values.slice(0, -1)}]]}]}`;
-        const newScoring_url = "https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/suicide_2/predictions?version=2021-12-20";
-        return getToken((err) => console.log(err), function () {
+    function workNigga() {
+        const parseId = req.body.parse_id;
+        const parsing = Parse.findOne({where: {id: parseId}}).then((prs) => {
             let tokenResponse;
             try {
                 tokenResponse = JSON.parse(this.responseText);
             } catch (ex) {
                 // TODO: handle parsing exception
             }
-            apiPost(newScoring_url, tokenResponse.access_token, newPayload, function (resp) {
-                let parsedPostResponse;
-                try {
-                    Analysis.update({
-                        status: 'done',
-                        result: this.responseText
-                    }, {
-                        where: {
-                            id: analId
-                        }
-                    });
-                } catch (ex) {
-                    // TODO: handle parsing exception
-                }
-            }, function (error) {
-                console.log(error);
-            });
-        });
-    }
-
-    const parseId = req.query.parseId;
-    const parsing = Parse.findOne({where: {id: parseId}}).then((prs) => {
-        getToken((err) => console.log(err), function () {
-            let tokenResponse;
-            try {
-                tokenResponse = JSON.parse(this.responseText);
-            } catch (ex) {
-                // TODO: handle parsing exception
-            }
-            const payload = `{"input_data": [{"fields": ["id", "text", "class"], "values": [[ null, "${prs.messages}", null]]}]}`;
-            const scoring_url = "https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/suicide/predictions?version=2021-12-20";
+            const payload = `{"Inputs":{"input1":[{"id":0,"text":"${prs.messages}","class":"null"}]}}`;
+            const scoring_url = config.constants.AZURE_URL;
             // ibm hours r dead, so now we'll do that lmao
-            Analysis.update({
-                status: 'done',
-                result: JSON.stringify({
-                    "predictionFields": ["Category_attempted to attempt",
-                        "Category_disease/mental disorders/affective disorder",
-                        "Category_disease/mental disorders/self-destructive behavior/suicide",
-                        "Category_disease/nervous system diseases/bad",
-                        "Category_disease/nervous system diseases/pain",
-                        "Category_movies/film genres/anime/animal/invertebrates",
-                        "Category_people",
-                        "Category_psychology/behavior and behavior mechanisms/emotions",
-                        "Category_psychology/behavior and behavior mechanisms/emotions/love",
-                        "Category_psychology/psychological phenomena and processes/mental processes/learning/better"],
-                    "predictionValues": [Array.from({length: 10}, () => Math.random())],
-                })
-            }, {
-                where: {
-                    id: analId
-                }
-            });
-            apiPost(scoring_url, tokenResponse.access_token, payload, function (resp) {
-                let parsedPostResponse;
-                try {
-                    parsedPostResponse = JSON.parse(this.responseText);
+            req = new XMLHttpRequest();
+            req.open('post', scoring_url);
+            req.setRequestHeader("Accept", "application/json");
+            req.setRequestHeader('authorization', `Bearer ${config.azureToken}`);
+            req.onreadystatechange = function () {
+                if (req.readyState == 4 && req.status == 200) {
+                    let parsedPostResponse;
+                    try {
+                        parsedPostResponse = JSON.parse(this.responseText);
 
-                    let meme = getFinal(parsedPostResponse, tokenResponse);
-                } catch (ex) {
-                    // TODO: handle parsing exception
+                        Analysis.update({
+                            status: 'done',
+                            result: this.responseText,
+                        }, {
+                            where: {
+                                id: analId
+                            }
+                        });
+                    } catch (ex) {
+                        console.log(ex);
+                        // TODO: handle parsing exception
+                    }
                 }
-            }, function (error) {
-                console.log(error);
-            });
+                else console.log(req.status)
+            };
+            req.send(payload);
         });
-
-    });
+    }
 };
 
 exports.parsingsById = (req, res) => {
